cb330c69277f9e94d2ca295f67280abf008e64ec
thaflavorsaver <dojohnson@ucdavis.edu>
2023-02-21 11:18:23 -0800

    added prisma, MUI, railway and wrote initial schemas for Postgres database

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link"
+import '@fontsource/public-sans'
 
 export default function Home() {
   return (
     <main className="py-8 px-48">
-      <Link className="bg-pink-700 hover:bg-blue-700 text-black font-medium py-2 px-4 rounded-md" href={"/dashboard"}>Dashboard</Link>
+      <Link className="bg-blue-900 hover:bg-blue-700 text-black font-medium py-2 px-4 rounded-md" href={"/dashboard"}>Dashboard</Link>
     </main>
   )
 };
